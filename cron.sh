c8157a9d1646af52902fcae8e8633a7217666024
Daniel Galvao Guerra <t-daguer@microsoft.com>
2017-07-19 16:10:16 -0700

    POST method and cron job should work now

--- cron.sh
+++ cron.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-now=$(date +"%T")
-echo "Current time : $now"
 
+# Gets the filename of the file in inputFiles that was first put there, and if it's not blank, call train.py and pass the file path
+
+LATEST_FILENAME=$(ls -ltr ../app/inputFiles | awk '{print $9}' | grep -Ev ^$ | head -1)
+
+if ! [ -z $LATEST_FILENAME ] # if filename not empty 
+then
+	FILEPATH="inputFile/"$LATEST_FILENAME
+	python src/train.py $FILEPATH
+	echo "Called train.py on file "$LATEST_FILENAME
+else
+	echo "There were no files"
+fi
