ea20b23033dab0d9b711e4d6f0ccf525e7f1cae7
Daniel Galvao Guerra <t-daguer@microsoft.com>
2017-07-20 09:42:48 -0700

    Updated buildRunCheck

--- buildRunCheck.sh
+++ buildRunCheck.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-IMG_NAME=pred-service
+docker build -t flask-tutorial:latest .
+docker stop predictapi
+docker rm predictapi
+docker run -i -p 5000:5000 \
+	--name predictapi  \
+	-v /Users/t-daguer/Documents/theoracle:/app/src \
+	-d flask-tutorial
 
-USAGE="Usage: buildRunCheck.sh [-img_name IMGNAME]"
-
-while [[ "$#" -gt 1 ]]
-do
-	arg="$1"
-	case $arg in
-	-img_name)
-		IMG_NAME="$2"
-		shift
-		;;
-	-u|-usage)
-		echo $USAGE
-		exit 1
-		;;
-	esac
-	shift
-done
-
-docker build -t $IMG_NAME:latest .
-docker run -d -p 5000:5000 $IMG_NAME
-docker ps
+#sleep 1s # this is used because when containers don't work they quit after 1 sec and don't show up on docker ps
+#docker ps
 
 exit 0
