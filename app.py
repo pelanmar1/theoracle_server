ecd787cffaa1a892d2d6b504a0296e517e8e8d55
Daniel Galvao Guerra <t-daguer@microsoft.com>
2017-07-20 09:40:46 -0700

    Updated files

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request
+import pandas
 import os
 import md5
 
 UPLOAD_FOLDER = 'inputFiles'
 ALLOWED_EXTENSIONS = set(['csv'])
 
+modelsFolder = 'models'
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Right now only checks extension but can be edited to check any number of things abour the file to validate it
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Saves job file to specified location
+def enqueueJob(df):
+    rid=md5.md5(str(df)).hexdigest()
+    df.to_csv('inputFiles/'+rid+'.csv')
+    return rid
 
 @app.route('/')
 def dataForm():
@@ -20,35 +30,25 @@ def handlePost():
     if request.method == 'POST':
         file = request.files['file']
 	if file and allowed_file(file.filename):
-	    rid = md5.md5(str(file.read())).hexdigest()
-	    filename = rid+os.path.splitext(file.filename)[1] #[1] is the extension of the file
-	    file.stream.seek(0) # Go back to the start of the file
-	    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return filename+'\n'
-	    #return "Ok"
+	    file.save('tempFiles/'+file.filename)
+	    df = pandas.read_csv('tempFiles/'+file.filename)
+	    rid = enqueueJob(df)
+            return rid + '\n'
+	    #rid = md5.md5(str(file.read())).hexdigest()
+	    #filename = rid+os.path.splitext(file.filename)[1] #[1] is the extension of the file
+	    #file.stream.seek(0) # Go back to the start of the file
+	    #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #return filename+'\n'
 
     return "not ok"
 
-    #x_in = request.form['xData']
-    #y_in = request.form['yData']
-    
-    #csvFile = request.form['fileBtn']
-    #return csvFile
-    #df = pd.read_csv(csvFile)
-    #return "Ok"
-    #print(df)
-    
-    #data = {'x':[1,2,3]}
-    #return """
-    #<html>
-    #    <head>
-    #        <title>Prediction Service</title>
-    #    </head>
-    #    <body>
-    #        <h1>Subtitle</h1>
-    #	</body>
-    #</html>"""
-    #return render_template('index.html')
+def getReqStatus(rid):
+    return (False, 'Ok') 
+
+@app.route('/status/<string:rid>', methods=['GET'])
+def handleRequest():
+    return rid
+    #(done, msg) = getReqStatus(rid)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
