cc3200b81cb09e8feb5fbae6670f18970a6c94ed
Pedro Lanzagorta <t-pelanz@Temps-MBP.guest.corp.microsoft.com>
2017-08-07 15:58:54 -0700

    I refactor file for easier startup

--- buildRunCheck.sh
+++ buildRunCheck.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-docker build -t flask-tutorial:latest .
+docker build -t predictapi:latest .
 docker stop predictapi
 docker rm predictapi
 docker run -i -p 5000:5000 \
 	--name predictapi  \
-	-v /Users/t-daguer/Documents/theoracle:/app/src \
-	-d flask-tutorial
+	-v /Users/t-pelanz/Documents/Microsoft/theoracle:/app/src \
+	-d predictapi
 
 #sleep 1s # this is used because when containers don't work they quit after 1 sec and don't show up on docker ps
 #docker ps
 
 exit 0
+
+'
+
+
+'
